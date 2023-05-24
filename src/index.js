c3f1d941f5992e74c5963ca99a97296ed7551065
Tim Kindregan <timothyj.kindregan@gmail.com>
2023-05-24 19:48:58 -0400

    commit tree 281627ad58ef8d96f6036bbdc26f97c45afc59c7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleAddTask(e.target['new-task-description'].value);
+    form.reset();
+  })
 });
+
+function handleAddTask (task) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = ('x')
+  p.textContent = `${task} `;
+  p.appendChild(btn);
+  document.querySelector('#list').appendChild(p);
+}
+
+function deleteTask (e) {
+  e.target.parentNode.remove()
+}
